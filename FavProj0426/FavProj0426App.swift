116374b4081e2f32ba1cdca2ed3dbd170dc9de72
张亚飞 <672233580@qq.com>
2022-04-26 12:47:05 +0800

    Initial Commit

---
+++ FavProj0426/FavProj0426App.swift
@@ -0,0 +1,17 @@
+//
+//  FavProj0426App.swift
+//  FavProj0426
+//
+//  Created by 张亚飞 on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FavProj0426App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
